794434ecf9ff46dd3a82784f5d87a0276f439830
Maksym Zakshevskyi <korobokvk@gmail.com>
2020-01-27 10:19:22 +0200

    Rabdom data geberator

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,40 @@
 import { Injectable } from '@nestjs/common'
 
+export interface RandomData {
+  delimiter: Number
+  data: Array<{ label: String; value: Number }>
+}
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World from data generator!'
+  generateRandomData(): RandomData {
+    return {
+      delimiter: Math.round(Math.random() * 10),
+      data: [
+        {
+          label: 'lab1',
+          value: Math.round((Math.random() + 1) * 10),
+        },
+        {
+          label: 'lab2',
+          value: Math.round((Math.random() + 1) * 10),
+        },
+        {
+          label: 'lab3',
+          value: Math.round((Math.random() + 1) * 10),
+        },
+        {
+          label: 'lab4',
+          value: Math.round((Math.random() + 1) * 10),
+        },
+        {
+          label: 'lab5',
+          value: Math.round((Math.random() + 1) * 10),
+        },
+      ],
+    }
+  }
+
+  getRandomData(): RandomData {
+    return this.generateRandomData()
   }
 }
