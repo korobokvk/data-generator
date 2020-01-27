a4ee25585a773656bc596c23344214029627f2f3
Maksym Zakshevskyi <mzakshevskyi@lohika.com>
2020-01-28 00:09:46 +0200

    setup CI

--- src/app.service.spec.ts
+++ src/app.service.spec.ts
@@ -1,22 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing'
-import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
-describe('AppController', () => {
-  let appController: AppController
+describe('AppService', () => {
+  let appService: AppService
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
       providers: [AppService],
     }).compile()
 
-    appController = app.get<AppController>(AppController)
+    appService = app.get<AppService>(AppService)
   })
 
   describe('root', () => {
     it('should return random data', () => {
-      expect(appController.getData()).toBeInstanceOf(Object)
+      expect(appService.getRandomData()).toBeInstanceOf(Object)
     })
   })
 })
